6b9be1ef4ffe2a822ce4063eff8ece57c13d0a4b
ardianilyas <ardianilyas@gmail.com>
2026-02-13 15:22:15 +0700

    fix: update UI

--- app/components/Skills.vue
+++ app/components/Skills.vue
@@ -10,6 +10,7 @@
 
     <div class="container mx-auto border-x border-gray-200/80 dark:border-gray-800">
 
+      
       <!-- GRID HEADER -->
       <div 
         class="hidden md:grid md:grid-cols-2 border-b border-gray-200 dark:border-gray-800 px-6 md:px-16 py-6 
@@ -20,13 +21,18 @@
 
       <!-- SKILL ROWS -->
       <SkillRow 
+        label="Programming Languages"
+        tech="JavaScript, TypeScript, PHP, Go"
+      />
+
+      <SkillRow 
         label="Frontend"
         tech="Vue.js, NuxtJS, React, NextJS, TailwindCSS, Shadcn UI"
       />
 
       <SkillRow 
         label="Backend"
-        tech="Node.js, Express, Laravel, Go, Gin"
+        tech="Express, Laravel, Gin, InertiaJS"
       />
       
       <SkillRow 
@@ -46,7 +52,7 @@
 
       <SkillRow 
         label="Tools"
-        tech="VSCode, Git, Postman, TablePlus, Safari"
+        tech="VSCode, Figma, Git, Postman, TablePlus, Safari"
         :last="true"
       />
 
